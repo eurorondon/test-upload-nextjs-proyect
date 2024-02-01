0618d4bfa2f2b059b228599affe8a051d063e0b7
eurorondon03 <eurorondon03@gmail.com>
2024-01-31 23:22:36 -0400

    Done

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,18 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
+import AchievementsSection from "./components/AchievementsSection";
+import ProjectsSection from "./components/ProjectsSection";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <main className="flex flex-col mx-auto min-h-screen bg-[#121212] container px-12 py-4 ">
-      <HeroSection />
+    <main className="flex min-h-screen flex-col bg-[#121212] ">
+      <Navbar />
+      <div className="container mt-24 mx-auto px-12 py-4">
+        <HeroSection />
+        <AchievementsSection />
+        <ProjectsSection />
+      </div>
     </main>
   );
 }
